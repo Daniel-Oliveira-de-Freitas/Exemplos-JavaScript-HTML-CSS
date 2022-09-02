d1a33bc05d564a3655daf83b70e6f400f1abe4f8
2010101289 <danielodf2.aluno@unipampa.edu.br>
2022-09-02 16:23:08 -0300

    começando aula10

---
+++ aula10/main.js
@@ -0,0 +1,31 @@
+ function eventoClick() {
+    document.body.style.backgroundColor = "blue";
+ }
+
+ function doisClick(){
+    alert("doisClick")
+ }
+
+ function eventoSaideCima(){
+    document.body.style.backgroundColor = "#ffffff"
+ }
+
+ function eventoEmCima(){
+    document.body.style.backgroundColor = "yellow"
+ }
+
+ function viraBlue(){
+    let red = document.getElementById("cont")
+    red.style.backgroundColor = "blue"
+ }
+
+ function viraWith(){
+    let red = document.getElementById("cont")
+    red.style.backgroundColor = "#ffffff"
+ }
+
+ function adicionaTexto(){
+    let p = document.getElementById("cont")
+    p.append('O mouse moveu <br>')
+ }
+
