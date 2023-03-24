eddaa556dbd75878c7baa079761eadb6ba876ba4
Daniel-Oliveira-de-Freitas <danisnake27@gmail.com>
2023-03-24 02:50:48 -0300

    Criando pagina inicial blog usando a API Fetch

---
+++ aula10/index.js
@@ -0,0 +1,21 @@
+ /*
+              EVENTOS JavaScript
+
+ onmouseover -> disparado quando o mouse está sobre.
+ onmouseoutr -> disparado quando o mouse é movido para fora do elemento.
+ onmousemove -> disparado quando o mouse é movido no elemento.
+ onmousedown -> disparado quando o clique do botão é precionado.
+ onmouseup -> disparado quando o clique do botão é liberado.
+ onfocus -> disparado quando o elemento recebe o foco. valido para input,
+ onchance -> disparado quando existe uma mudança no conteudo. "Ao mudar".
+ onblur -> disparado quando o elemento perde o foco.
+ onkeydown -> disparado quando uma tecla é precionada e solta.
+ onkeyup -> disparado quando uma tecla é solta sobre um elemento.
+ onload -> disparado quando a página terminou de ser carregada. Body.
+ onresize -> disparado quando há um redirecionamento da janela.
+
+ */
+
+ var m = "Ola mundo!";
+ console.log(m);
+ document.getElementById("text").innerHTML = m;
---
+++ ex/main.js
@@ -0,0 +1,6 @@
+function funcao1(){
+    alert ("Olá eu sou um alerta JS");
+    var nome = prompt("Digite seu nome");
+    document.getElementById('title').innerHTML = nome;
+    
+}
